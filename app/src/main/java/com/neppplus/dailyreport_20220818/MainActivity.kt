1fcda7dd08a5f64b5af1ce3ff09f4f27e3cbe3a2
넵플러스 <kmc901206@gmail.com>
2022-08-18 15:33:57 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/dailyreport_20220818/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.neppplus.dailyreport_20220818
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
