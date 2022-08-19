d3f832626a13490c92bc04340f2ac1107bdc9b13
넵플러스 <kmc901206@gmail.com>
2022-08-19 14:56:27 +0900

    내부 리싸이클러뷰 바인딩 활용

--- app/src/main/java/com/neppplus/dailyreport_20220818/adapters/GoalRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/dailyreport_20220818/adapters/GoalRecyclerAdapter.kt
@@ -9,25 +9,25 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.neppplus.dailyreport_20220818.R
+import com.neppplus.dailyreport_20220818.databinding.GoalListItemBinding
 import com.neppplus.dailyreport_20220818.datas.GoalData
 
 class GoalRecyclerAdapter(
     val mContext: Context, val mList : List<GoalData>
 ): RecyclerView.Adapter<GoalRecyclerAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class MyViewHolder(
+        val binding : GoalListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: GoalData) {
-            val goalColorImg = itemView.findViewById<ImageView>(R.id.goalColorImg)
-            val goalTitleTxt = itemView.findViewById<TextView>(R.id.goalTitleTxt)
-
-            goalTitleTxt.text = item.title
-            goalColorImg.background.setTint(Color.parseColor(item.color))  // Color.getColor
+            binding.goalTitleTxt.text = item.title
+            binding.goalColorImg.background.setTint(Color.parseColor(item.color))
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val row = LayoutInflater.from(mContext).inflate(R.layout.goal_list_item, parent, false)
-        return MyViewHolder(row)
+        val binding = GoalListItemBinding.inflate(LayoutInflater.from(mContext), parent, false)
+        return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
